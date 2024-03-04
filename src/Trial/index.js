89e8a69f996e0489a60dd0e740fb1e3ea30fdce2
Aditya Gurnani <adityagurnani@gmail.com>
2024-03-03 19:03:30 -0500

    Update index.js

--- src/Trial/index.js
+++ src/Trial/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Trial({ dummyAccounts }) {
+function Trial() {
   return (
     <div>
       <h2>Welcome to our Gym Web Application Trial</h2>
@@ -9,13 +9,7 @@ function Trial({ dummyAccounts }) {
       </p>
       <h3>Dummy Account Details:</h3>
       <ul>
-        {dummyAccounts.map((account, index) => (
-          <li key={index}>
-            <strong>User Type:</strong> {account.userType}<br/>
-            <strong>User ID:</strong> {account.userId}<br/>
-            <strong>Password:</strong> {account.password}
-          </li>
-        ))}
+       
       </ul>
     </div>
   );
